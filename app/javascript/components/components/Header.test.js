d7baeb3b7670801524e3944d5f78a0cfcd429640
ecostrong <necoarmstrong@mac.com>
2022-05-18 09:51:35 -0700

    more changes

--- app/javascript/components/components/Header.test.js
+++ app/javascript/components/components/Header.test.js
@@ -13,10 +13,10 @@ import Header from './Header'
 //Allows us to utilize the adapter we import in earlier, allowing us to call and render a component.
 Enzyme.configure({ adapter: new Adapter() })
 
-describe("When Header Reanders", () => {
+describe("When Header Renders", () => {
   it("displays a NavItem in Header", () => {
     const header = shallow(<Header />)
-    const headerHeading = header.find("NavItem")
-    expect(headerHeading.text()).toEqual("3")
+    const headerHeading = header.find("h3")
+    expect(headerHeading.text()).toEqual("header")
   })
 })
